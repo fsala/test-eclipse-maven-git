f4db15dc14b164fa04e566dd3706e181f4c07bfa
fsala <fsala@segucom.net>
2014-07-11 11:21:11 +0200

    test

---
+++ src/main/java/com/segucom/test/test_eclipse_maven_git/App.java
@@ -0,0 +1,13 @@
+package com.segucom.test.test_eclipse_maven_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
